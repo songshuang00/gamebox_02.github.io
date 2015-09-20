e15fc039cb3bb63bf0c68f5649337461c489b6d2
Shuang Song <songshuang00@users.noreply.github.com>
2015-09-20 16:57:37 +0800

    A plane-fighting game was completely created in Game1.js

--- Game/game1.js
+++ Game/game1.js
@@ -1,4 +1,4 @@
-$(document).ready(
+$(document).ready(function(){
     require(
         ['zrender/zrender'],
         function(zrender) {
@@ -9,10 +9,10 @@ $(document).ready(
     var Droplet=require(zrender/shape/Droplet);
     var probeImage=new Droplet({
         style:{
-                x:730,
-                y:275,
-                a:30,
-                b:30,
+                x:275,
+                y:730,
+                a:20,
+                b:20,
                 color:'#DCDCDC',
                 lineWidth:1,
                 shadowColor:'#696969',
@@ -28,20 +28,6 @@ $(document).ready(
                 color:'#FFFFFF',
                 lineWidth:0,
                 }
-        });
-                  
-    var Rectangle=require(zrender/shape/Rectangle);
-    var rockImage=new Rectangle({
-        style:{
-                width:30,
-                height:30,
-                color:'#FFA500',
-                lineWidth:1,
-                radius:3,
-                shadowColor:'#696969',
-                shadowOffsetX:15,
-                }
-                  
         });
      
     var Heart=require(zrender/shape/Heart);
@@ -67,40 +53,136 @@ $(document).ready(
                 }
         });
                   
-    var gScore, gOver;
-                  
-    zr.addShape(probeImage);
-    
-    define(function(){
+    var gScore=0, gHP=3;
+                          
+    $('#gcanvas').ready(function(){
+        $(this).click(function(){$(this).fadeIn()});
         
-    })
-                
-    
+        zr.addShape(new probeImage);
+       
+        $(this).keydown(function(e){
+            switch (event.which){
+                case 65:
+                {
+                  $(this.probeImage).animate({left:'1000px',},'slow',);
+                  if ($(this.probeImage.x)<=20)
+                      {
+                       $(this.probeImage).stop();
+                      };
+                };
+                case 83:
+                {
+                  $(this.probeImage).animate({bottom:'1000px',},'slow',);
+                  if ($(this.probeImage.y)>=730)
+                      {
+                       $(this.probeImage).stop();
+                      };
+                };
+                case 68:
+                {
+                  $(this.probeImage).animate({right:'1000px',},'slow',);
+                  if ($(this.probeImage.x)>=530)
+                      {
+                       $(this.probeImage).stop();
+                      };
+                };
+                case 87:
+                {
+                  $(this.probeImage).animate({top:'1000px',},'slow',);
+                  if ($(this.probeImage.y)<=20)
+                      {
+                       $(this.probeImage).stop();
+                      };
+                };
+                case 229:
+                {
+                 zr.addShape(new bulletImage({
+                     style:{
+                         x:this.probeImage.x,
+                         y:this.probeImage.y-20,
+                     }
+                  }));
                   
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  })
-                  
+                 $(this.bulletImage).animate({top:'1000px',},'fast',);
+                };
+                default:
+                    $(this.probeImage).stop();
+            };
             
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-);
+            $(this).define(function getRandom(n){
+                return Math.floor(Math.random()*n+1)
+                });
+                       
+            $(this).define(function addEnemy(){
+                zr.addShape(new enemyImage({
+                    style:{
+                        x:getRandom(520),
+                        y:30,
+                    }
+                }));
+            });
+        });
+    });
+            
+            var setIntervalFun1=null;
+            setIntervalFun1=setInterval('addEnemy()',1000*5);
+            $(this.enemyImage).animate({bottom:'1000px',},'normal',);
+            $(this.enemyImage).animate({left:'1000px',},getRandom(2000)-getRandom(2000)+1,);
+            if(setIntervalFun1 != null){
+                clearInterval(setIntervalFun1);
+            };
+            
+            function addHeart(){
+                zr.addShape(new heartImage({
+                    style:{
+                        x:getRandom(520),
+                        y:getRandom(720),
+                    }
+                }));
+            };
 
-        
-        
+            var setIntervalFun2=null;
+            setIntervalFun2=setInterval('addEnemy()',1000*50);
+            if(setIntervalFun2 != null){
+                clearInterval(setIntervalFun2);
+            };
+                 
+            var peDistance=Math.pow(this.probeImage.x-this.enemyImage.x,2)+Math.pow(this.probeImage.y-this.enemyImage.y,2);
+            var beDistance=Math.pow(this.bulletImage.x-this.enemyImage.x,2)+Math.pow(this.bulletImage.y-this.enemyImage.y,2);
+            var hpDistance=Math.pow(this.probeImage.x-this.heartImage.x,2)+Math.pow(this.probeImage.y-this.heartImage.y,2);
+            
+            if (peDistance<=2500){
+                gHP=gHP-1;
+            };
+            
+            if (beDistance<=9000){
+                $(this.enemyImage).remove();
+                $(this.bulletImage).remove();
+                gScore=gScore+1;  
+            };
+                 
+            if (hpDistance<=2500 && gHP<3){
+                gHP=gHP+1;
+                $(this.heartImage).remove();
+            };
+                
+            if (hpDistance<=2500 && gHP==3){
+                $(this.heartImage).remove();
+            };
+            
+            $(this).define(function gameOver(){
+                var myText=require(zrender/shape/Text);
+                zr.addShape(new myText({
+                    style:{
+                        x:200,
+                        y:300,
+                        text:'Game Over. Score:'+gScore,
+                    }
+                }));
+            });
+                               
+            if (gHP==0){
+                $(this).stop();
+                $(this).gameOver();
+            };
+});
